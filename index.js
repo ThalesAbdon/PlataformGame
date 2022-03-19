f8ed6b305f0afce9516d39e0cfcfe66e6a2ae182
Thales <thales.sousufpi@gmail.com>
2022-03-19 10:15:20 -0300

    Gravidade adicionada!

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector("canvas");
 
-const c = canvas.getContext('2d')
+const c = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-class Player{
-    constructor(){
-        this.position = {
-            x: 100,
-            y: 100
-        }
-        this.width = 28
-        
-        this.height = 28
+const gravity = 1.2
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    
+    //velocidade do jogador
+    this.velocity = {
+        x:0,
+        y:1
     }
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+
+    this.width = 28;
+    this.height = 28;
     }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  //função que irá atualizar(em relação a movimento/posição/velocidade) o jogador
+  update(){
+    this.position.y += this.velocity.y  
+    this.draw()
+    if(this.position.y + this.height + this.velocity.y <= canvas.height)
+    this.velocity.y += gravity
+    else this.velocity.y = 0
+  }
+
 }
 
-const player = new Player()
-player.draw()
+const player = new Player();
+player.update();
 
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0,canvas.width, canvas.height)
+    player.update()    
+}
+
+animate()
